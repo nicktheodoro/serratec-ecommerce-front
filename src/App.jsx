2dd4a0ae7d391c4f95d3de1d1866eee23548ad69
Nicolas Theodoro <nicolastheodoro@gmail.com>
2021-06-22 07:45:49 -0300

    adicione sass como dependencia de projeto

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import PrimarySearchAppBar from "./components/Header";
 
+import Cart from "./pages/Cart";
+
 import "./global.css"
 
 function App() {
   return (
     <div className="App">
       <PrimarySearchAppBar />
+      <Cart />
     </div>
   );
 }
---
+++ src/pages/Cart/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+export default function Cart() {
+  return (
+    <div className={styles.container}>
+      <h1>Testando Sass</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique est
+        qui voluptates explicabo unde et repellat ratione a molestias facere.
+        Consectetur cumque nam id at reprehenderit laborum, asperiores sapiente
+        rem?
+      </p>
+    </div>
+  );
+}
